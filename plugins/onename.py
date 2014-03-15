97279fab38b63926d588935b58fcccf7c04e88de
Michael Wozniak <github@koh.ms>
2014-03-14 20:49:07 -0400

    Basic functionality to lookup contacts at OneName
    basic functionality to lookup contacts from onename.  Currently only
    supports a button on the plugins settings window and only imports
    address and name fields.

---
+++ plugins/onename.py
@@ -0,0 +1,101 @@
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+from electrum_gui.qt.util import *
+from electrum.util import *
+from electrum.plugins import BasePlugin
+from electrum.i18n import _
+from electrum.bitcoin import is_valid
+
+import json
+import sys
+import httplib
+
+
+
+class Plugin(BasePlugin):
+    def fullname(self):
+        return 'OneName Contacts'
+
+    def description(self):
+        return 'Import contacts from the Namecoin blockchain.'
+
+    def __init__(self, gui, name):
+        BasePlugin.__init__(self, gui, name)
+        self._is_available = True
+
+    def init(self):
+        self.win = self.gui.main_window
+
+
+    def requires_settings(self):
+        return True
+
+    def settings_widget(self, window):
+        return EnterButton(_('Get Contact'), self.onename_contact_dialog)
+
+
+    def get_json(self, site, get_string):
+        try:
+            connection = httplib.HTTPSConnection(site)
+            connection.request("GET", get_string)
+        except Exception:
+            raise
+        resp = connection.getresponse()
+        if resp.reason == httplib.responses[httplib.NOT_FOUND]:
+            raise
+        try:
+            json_resp = json.loads(resp.read())
+        except Exception:
+            raise
+        return json_resp
+
+
+    def onename_contact_dialog(self):
+       
+        d = QDialog(self.win)
+        vbox = QVBoxLayout(d)
+        vbox.addWidget(QLabel(_('OneName Contact')+':'))
+
+        grid = QGridLayout()
+        line1 = QLineEdit()
+        line2 = QLineEdit()
+        grid.addWidget(QLabel(_("Username")), 1, 0)
+        grid.addWidget(line1, 1, 1)
+
+        vbox.addLayout(grid)
+        vbox.addLayout(ok_cancel_buttons(d))
+
+        if not d.exec_():
+            return
+
+        username = str(line1.text())
+
+        rjson1 = self.get_json('www.onename.io', "/" + username + ".json")
+        rjson2 = self.get_json('dns.dnschain.net', "/u/" + username)
+
+        address1 = rjson1['bitcoin']['address']
+        address2 = rjson2['bitcoin']['address']
+
+        try:
+            label = rjson1['name']['formatted']
+        except Exception:
+            pass
+
+        if address1 != address2:
+            QMessageBox.warning(self, _('Error'), _('Error getting match'), _('OK'))
+            return
+        else:
+            address = address1
+            
+        if not is_valid(address):
+            QMessageBox.warning(self, _('Error'), _('Invalid Address'), _('OK'))
+            return
+
+        self.win.wallet.add_contact(address)
+        if label:
+            self.win.wallet.set_label(address, label)
+
+        self.win.update_contacts_tab()
+        self.win.update_history_tab()
+        self.win.update_completions()
+        self.win.tabs.setCurrentIndex(3) 
