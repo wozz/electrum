ea85837aea46f4da9549bb7fe48a90c78b3c8f0b
Michael Wozniak <github@koh.ms>
2014-09-06 08:38:26 -0400

    move hist calculations to a thread

--- plugins/exchange_rate.py
+++ plugins/exchange_rate.py
@@ -425,94 +425,99 @@ class Plugin(BasePlugin):
     @hook
     def history_tab_update(self):
         if self.config.get('history_rates', 'unchecked') == "checked":
-            cur_exchange = self.config.get('use_exchange', "Blockchain")
+            hist_update_thread = threading.Thread(target=self.hist_update_async, args=(), kwargs={})
+            hist_update_thread.start()
+
+
+    def hist_update_async(self):
+        cur_exchange = self.config.get('use_exchange', "Blockchain")
+        try:
+            tx_list = self.tx_list
+        except Exception:
+            return
+
+        try:
+            mintimestr = datetime.datetime.fromtimestamp(int(min(tx_list.items(), key=lambda x: x[1]['timestamp'])[1]['timestamp'])).strftime('%Y-%m-%d')
+        except Exception:
+            return
+        maxtimestr = datetime.datetime.now().strftime('%Y-%m-%d')
+
+        if cur_exchange == "CoinDesk":
             try:
-                tx_list = self.tx_list
+                resp_hist = self.exchanger.get_json('api.coindesk.com', "/v1/bpi/historical/close.json?start=" + mintimestr + "&end=" + maxtimestr)
             except Exception:
                 return
-
+        elif cur_exchange == "Winkdex":
             try:
-                mintimestr = datetime.datetime.fromtimestamp(int(min(tx_list.items(), key=lambda x: x[1]['timestamp'])[1]['timestamp'])).strftime('%Y-%m-%d')
+                resp_hist = self.exchanger.get_json('winkdex.com', "/api/v0/series?start_time=1342915200")['series'][0]['results']
             except Exception:
                 return
-            maxtimestr = datetime.datetime.now().strftime('%Y-%m-%d')
+        elif cur_exchange == "BitcoinVenezuela":
+            cur_currency = self.fiat_unit()
+            if cur_currency == "VEF":
+                try:
+                    resp_hist = self.exchanger.get_json('api.bitcoinvenezuela.com', "/historical/index.php?coin=BTC")['VEF_BTC']
+                except Exception:
+                    return
+            elif cur_currency == "ARS":
+                try:
+                    resp_hist = self.exchanger.get_json('api.bitcoinvenezuela.com', "/historical/index.php?coin=BTC")['ARS_BTC']
+                except Exception:
+                    return
+            else:
+                return
 
+        self.gui.main_window.is_edit = True
+        self.gui.main_window.history_list.setColumnCount(6)
+        self.gui.main_window.history_list.setHeaderLabels( [ '', _('Date'), _('Description') , _('Amount'), _('Balance'), _('Fiat Amount')] )
+        root = self.gui.main_window.history_list.invisibleRootItem()
+        childcount = root.childCount()
+        for i in range(childcount):
+            item = root.child(i)
+            try:
+                tx_info = tx_list[str(item.data(0, Qt.UserRole).toPyObject())]
+            except Exception:
+                newtx = self.wallet.get_tx_history()
+                v = newtx[[x[0] for x in newtx].index(str(item.data(0, Qt.UserRole).toPyObject()))][3]
+
+                tx_info = {'timestamp':int(time.time()), 'value': v }
+                pass
+            tx_time = int(tx_info['timestamp'])
             if cur_exchange == "CoinDesk":
+                tx_time_str = datetime.datetime.fromtimestamp(tx_time).strftime('%Y-%m-%d')
                 try:
-                    resp_hist = self.exchanger.get_json('api.coindesk.com', "/v1/bpi/historical/close.json?start=" + mintimestr + "&end=" + maxtimestr)
-                except Exception:
-                    return
+                    tx_USD_val = "%.2f %s" % (Decimal(str(tx_info['value'])) / 100000000 * Decimal(resp_hist['bpi'][tx_time_str]), "USD")
+                except KeyError:
+                    tx_USD_val = "%.2f %s" % (self.btc_rate * Decimal(str(tx_info['value']))/100000000 , "USD")
             elif cur_exchange == "Winkdex":
+                tx_time_str = datetime.datetime.fromtimestamp(tx_time).strftime('%Y-%m-%d') + "T16:00:00-04:00"
                 try:
-                    resp_hist = self.exchanger.get_json('winkdex.com', "/api/v0/series?start_time=1342915200")['series'][0]['results']
-                except Exception:
-                    return
+                    tx_rate = resp_hist[[x['timestamp'] for x in resp_hist].index(tx_time_str)]['price']
+                    tx_USD_val = "%.2f %s" % (Decimal(tx_info['value']) / 100000000 * Decimal(tx_rate)/Decimal("100.0"), "USD")
+                except ValueError:
+                    tx_USD_val = "%.2f %s" % (self.btc_rate * Decimal(tx_info['value'])/100000000 , "USD")
+                except KeyError:
+                    tx_USD_val = _("No data")
             elif cur_exchange == "BitcoinVenezuela":
-                cur_currency = self.fiat_unit()
-                if cur_currency == "VEF":
-                    try:
-                        resp_hist = self.exchanger.get_json('api.bitcoinvenezuela.com', "/historical/index.php?coin=BTC")['VEF_BTC']
-                    except Exception:
-                        return
-                elif cur_currency == "ARS":
-                    try:
-                        resp_hist = self.exchanger.get_json('api.bitcoinvenezuela.com', "/historical/index.php?coin=BTC")['ARS_BTC']
-                    except Exception:
-                        return
-                else:
-                    return
-
-            self.gui.main_window.is_edit = True
-            self.gui.main_window.history_list.setColumnCount(6)
-            self.gui.main_window.history_list.setHeaderLabels( [ '', _('Date'), _('Description') , _('Amount'), _('Balance'), _('Fiat Amount')] )
-            root = self.gui.main_window.history_list.invisibleRootItem()
-            childcount = root.childCount()
-            for i in range(childcount):
-                item = root.child(i)
+                tx_time_str = datetime.datetime.fromtimestamp(tx_time).strftime('%Y-%m-%d')
                 try:
-                    tx_info = tx_list[str(item.data(0, Qt.UserRole).toPyObject())]
-                except Exception:
-                    newtx = self.wallet.get_tx_history()
-                    v = newtx[[x[0] for x in newtx].index(str(item.data(0, Qt.UserRole).toPyObject()))][3]
+                    num = resp_hist[tx_time_str].replace(',','')
+                    tx_BTCVEN_val = "%.2f %s" % (Decimal(str(tx_info['value'])) / 100000000 * Decimal(num), cur_currency)
+                except KeyError:
+                    tx_BTCVEN_val = _("No data")
 
-                    tx_info = {'timestamp':int(time.time()), 'value': v }
-                    pass
-                tx_time = int(tx_info['timestamp'])
-                if cur_exchange == "CoinDesk":
-                    tx_time_str = datetime.datetime.fromtimestamp(tx_time).strftime('%Y-%m-%d')
-                    try:
-                        tx_USD_val = "%.2f %s" % (Decimal(str(tx_info['value'])) / 100000000 * Decimal(resp_hist['bpi'][tx_time_str]), "USD")
-                    except KeyError:
-                        tx_USD_val = "%.2f %s" % (self.btc_rate * Decimal(str(tx_info['value']))/100000000 , "USD")
-                elif cur_exchange == "Winkdex":
-                    tx_time_str = datetime.datetime.fromtimestamp(tx_time).strftime('%Y-%m-%d') + "T16:00:00-04:00"
-                    try:
-                        tx_rate = resp_hist[[x['timestamp'] for x in resp_hist].index(tx_time_str)]['price']
-                        tx_USD_val = "%.2f %s" % (Decimal(tx_info['value']) / 100000000 * Decimal(tx_rate)/Decimal("100.0"), "USD")
-                    except ValueError:
-                        tx_USD_val = "%.2f %s" % (self.btc_rate * Decimal(tx_info['value'])/100000000 , "USD")
-                    except KeyError:
-                        tx_USD_val = _("No data")
-                elif cur_exchange == "BitcoinVenezuela":
-                    tx_time_str = datetime.datetime.fromtimestamp(tx_time).strftime('%Y-%m-%d')
-                    try:
-                        num = resp_hist[tx_time_str].replace(',','')
-                        tx_BTCVEN_val = "%.2f %s" % (Decimal(str(tx_info['value'])) / 100000000 * Decimal(num), cur_currency)
-                    except KeyError:
-                        tx_BTCVEN_val = _("No data")
+            if cur_exchange in ["CoinDesk", "Winkdex"]:
+                item.setText(5, tx_USD_val)
+            elif cur_exchange == "BitcoinVenezuela":
+                item.setText(5, tx_BTCVEN_val)
+            if Decimal(str(tx_info['value'])) < 0:
+                item.setForeground(5, QBrush(QColor("#BC1E1E")))
 
-                if cur_exchange in ["CoinDesk", "Winkdex"]:
-                    item.setText(5, tx_USD_val)
-                elif cur_exchange == "BitcoinVenezuela":
-                    item.setText(5, tx_BTCVEN_val)
-                if Decimal(str(tx_info['value'])) < 0:
-                    item.setForeground(5, QBrush(QColor("#BC1E1E")))
-
-            for i, width in enumerate(self.gui.main_window.column_widths['history']):
-                self.gui.main_window.history_list.setColumnWidth(i, width)
-            self.gui.main_window.history_list.setColumnWidth(4, 140)
-            self.gui.main_window.history_list.setColumnWidth(5, 120)
-            self.gui.main_window.is_edit = False
+        for i, width in enumerate(self.gui.main_window.column_widths['history']):
+            self.gui.main_window.history_list.setColumnWidth(i, width)
+        self.gui.main_window.history_list.setColumnWidth(4, 140)
+        self.gui.main_window.history_list.setColumnWidth(5, 120)
+        self.gui.main_window.is_edit = False
 
 
     def settings_widget(self, window):
